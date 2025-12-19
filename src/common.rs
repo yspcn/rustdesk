ad2859c8bdd7c88712661ab00dfbbb1dba796b26
yspcn <exunpin@gmail.com>
2025-12-19 11:27:09 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://8.148.7.103:21114".to_owned()
 }
 
 #[inline]
